dfd16a367958eca720356cf6f44141a30525ec5a
James Vasquez <jamesvas5307@live.com>
2017-08-25 12:32:46 -0400

    Checkpoint 4

--- lib/kele.rb
+++ lib/kele.rb
@@ -13,4 +13,10 @@ class Kele
     r = self.class.get('https://www.bloc.io/api/v1/users/me', headers: { "authorization" => @auth_token })
     @profile = JSON.parse(r.body)
   end
+
+  def get_mentor_availability
+    mentor_id = @profile["current_enrollment"]["mentor_id"]
+    availability = self.class.get("https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
+    @mentor_availabile = JSON.parse(availability.body)
+  end
 end
