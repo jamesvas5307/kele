3b834b568431273dd3d14c3f6b709223b5297aac
James Vasquez <jamesvas5307@live.com>
2017-08-28 07:48:57 -0400

    Checkpoint 6

--- lib/kele.rb
+++ lib/kele.rb
@@ -8,8 +8,9 @@ class Kele
   include Roadmap
   def initialize (email,password)
     @api = "https://www.bloc.io/api/v1/sessions"
-
-    response = self.class.post(@api, query: { email: email, password: password })
+    @email = email
+    @password = password
+    response = self.class.post(@api, query: { email: @email, password: @password })
     @auth_token = response['auth_token']
   end
 
@@ -22,6 +23,22 @@ class Kele
     mentor_id = @profile["current_enrollment"]["mentor_id"]
     availability = self.class.get("https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
     @mentor_availabile = JSON.parse(availability.body)
+  end
+
+  def get_messages(v = nil)
+    url = "https://www.bloc.io/api/v1/message_threads"
+    messages = self.class.get(url, headers: { "authorization" => @auth_token }, values: {"page" => v})
+  end
+
+  def send_message
+    url = "https://www.bloc.io/api/v1/messages"
+    sendersEmail = @email
+    mentor_id = @profile["current_enrollment"]["mentor_id"]
+    puts "What would you like the subject of the message to be?"
+    subject = gets.chomp
+    puts "What would you like your message to be?"
+    message = gets.chomp
+    response = self.class.post(url, headers: { "authorization" => @auth_token }, values: {"sender"=> sendersEmail, "recipient_id" => mentor_id, "subject" => subject, "stripped-text" => message})
   end
 
 end
